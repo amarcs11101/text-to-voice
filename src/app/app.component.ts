1fa0be5c7337f433e17445e63b05fb3eb818c9f6
amarcs721992@gmail.com <amarcs721992@gmail.com>
2020-06-06 18:29:28 +0530

    text to voice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { HttpClient} from '@angular/common/http';
+import { LiteralMapEntry } from '@angular/compiler/src/output/output_ast';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'text-to-voice-app';
+  voiceMesssage : string;
+  message : string;
+  sayIt() {
+   this.message= (<HTMLInputElement>document.getElementById('textAreaMessage')).value;
+    window.speechSynthesis.speak(new SpeechSynthesisUtterance(this.message));
+  }
 }
